707975e6d6614ce7bf82c572e480efe093335ca5
Praveena <praveenareddykr@gmail.com>
2024-08-10 19:56:40 +0530

    just added comment

--- src/test/java/FrameworksDemo/testclasses/EndToEndClass.java
+++ src/test/java/FrameworksDemo/testclasses/EndToEndClass.java
@@ -41,6 +41,7 @@ public class EndToEndClass extends BaseTest{
 		Assert.assertTrue(con.getConfirm());
 		
 	}
+//just comment
 		@Test(dependsOnMethods= {"submitOrder"})
 		public void VerifyOrder() throws InterruptedException, IOException {
 		String Prodname="ZARA COAT 3";
